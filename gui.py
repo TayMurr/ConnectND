a8a8c79bf4af70e4843eaee46ec2af352f2d3346
Alan Flores <aflores8@student01.cse.nd.edu>
2017-04-12 22:49:34 -0400

    gui.py reverted to PyQt4

--- gui.py
+++ gui.py
@@ -4,38 +4,37 @@
 # GUI for the ConnectND app
 # Author: Alan Flores
 import sys
-from PyQt5.QtWidgets import QWidget, QCheckBox, QApplication
-from PyQt5.QtCore import Qt
+from PyQt4 import QtGui, QtCore
 
 
-class ConnectND(QWidget):
+class ConnectND(QtGui.QWidget):
 	
 	def __init__(self):
-		super().__init__()
+		super(ConnectNd, self).__init__()
 		self.initUI()
 		
 	def initUI(self):	  
-		dmcb = QCheckBox('Dorm', self) # setting checkboxes
+		dmcb = QTGui.QCheckBox('Dorm', self) # setting checkboxes
 		dmcb.move(400, 50)
 		dmcb.toggle()
 		dmcb.stateChanged.connect(self.changeBool1)
-		mjcb = QCheckBox('Major', self)
+		mjcb = QtGui.QCheckBox('Major', self)
 		mjcb.move(400, 100)
 		mjcb.toggle()
 		mjcb.stateChanged.connect(self.changeBool2)
-		clcb = QCheckBox('College', self)
+		clcb = QtGui.QCheckBox('College', self)
 		clcb.move(400, 150)
 		clcb.toggle()
 		clcb.stateChanged.connect(self.changeBool3)
-		yrcb = QCheckBox('Year', self)
+		yrcb = QtGui.QCheckBox('Year', self)
 		yrcb.move(400, 200)
 		yrcb.toggle()
 		yrcb.stateChanged.connect(self.changeBool4)
-		jbcb = QCheckBox('Job', self)
+		jbcb = QtGui.QCheckBox('Job', self)
 		jbcb.move(400, 250)
 		jbcb.toggle()
 		jbcb.stateChanged.connect(self.changeBool5)
-		hscb = QCheckBox('Home State', self)
+		hscb = QtGui.QCheckBox('Home State', self)
 		hscb.move(400, 300)
 		hscb.toggle()
 		hscb.stateChanged.connect(self.changeBool6)
@@ -46,32 +45,32 @@ class ConnectND(QWidget):
 
 		
 	def changeBool1(self, state): # function for a checkbox
-		if state == Qt.Checked:
+		if state == QtCore.Qt.Checked:
 			dm = True
 		else:
 			dm = False
 	def changeBool2(self, state):
-		if state == Qt.Checked:
+		if state == QtCore.Qt.Checked:
 			mj = True
 		else:
 			mj = False
 	def changeBool3(self, state):
-		if state == Qt.Checked:
+		if state == QtCore.Qt.Checked:
 			cl = True
 		else:
 			cl = False
 	def changeBool4(self, state):
-		if state == Qt.Checked:
+		if state == QtCore.Qt.Checked:
 			yr = True
 		else:
 			yr = False
 	def changeBool5(self, state):
-		if state == Qt.Checked:
+		if state == QtCore.Qt.Checked:
 			jb = True
 		else:
 			jb = False
 	def changeBool6(self, state):
-		if state == Qt.Checked:
+		if state == QtCore.Qt.Checked:
 			hs = True
 		else:
 			hs = False
